47c90f28c9f05f08b621d5a24eb3c216e7d951f1
fuchunhui <fuchunhui@baidu.com>
2021-04-08 18:32:11 +0800

    test: cconfig

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import svelte from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
   // base: process.env.NODE_ENV === 'production' ? '/blue/' : '/'
+  base: process.env.NODE_ENV === 'production' ? './' : '/'
 })
